d7156feed92c9a7e3a9cefd77a2bde1e2fe78586
polyphant <chrisl1991@hotmail.co.uk>
2015-08-14 17:25:36 +0100

    minor

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,37 +7,15 @@
 using namespace Rcpp;
 
 // ngram_generator
-std::vector<std::string> ngram_generator(std::string sentence, unsigned int n);
-RcppExport SEXP Narrative_ngram_generator(SEXP sentenceSEXP, SEXP nSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< std::string >::type sentence(sentenceSEXP);
-    Rcpp::traits::input_parameter< unsigned int >::type n(nSEXP);
-    __result = Rcpp::wrap(ngram_generator(sentence, n));
-    return __result;
-END_RCPP
-}
-// tdm_creator
-std::map<std::string, int> tdm_creator(std::vector<std::string> ngrams);
-RcppExport SEXP Narrative_tdm_creator(SEXP ngramsSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< std::vector<std::string> >::type ngrams(ngramsSEXP);
-    __result = Rcpp::wrap(tdm_creator(ngrams));
-    return __result;
-END_RCPP
-}
-// tdm_generator
-Rcpp::List tdm_generator(std::vector<std::string> documents, int n);
-RcppExport SEXP Narrative_tdm_generator(SEXP documentsSEXP, SEXP nSEXP) {
+std::vector<std::map<std::string, int> > ngram_generator(std::vector<std::string> documents, unsigned int n, char delim);
+RcppExport SEXP Narrative_ngram_generator(SEXP documentsSEXP, SEXP nSEXP, SEXP delimSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
     Rcpp::traits::input_parameter< std::vector<std::string> >::type documents(documentsSEXP);
-    Rcpp::traits::input_parameter< int >::type n(nSEXP);
-    __result = Rcpp::wrap(tdm_generator(documents, n));
+    Rcpp::traits::input_parameter< unsigned int >::type n(nSEXP);
+    Rcpp::traits::input_parameter< char >::type delim(delimSEXP);
+    __result = Rcpp::wrap(ngram_generator(documents, n, delim));
     return __result;
 END_RCPP
 }
--- src/ngram_generator.cpp
+++ src/ngram_generator.cpp
@@ -1,147 +1,53 @@
 #include "RcppArmadillo.h"
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// [[Rcpp::export(name=".ngram_generator")]]
-std::vector<std::string> ngram_generator(std::string sentence, unsigned int n){
-	std::istringstream iss(sentence);
+// [[Rcpp::export(name="ngram_generator")]]
+std::vector<std::map<std::string, int> > ngram_generator(std::vector<std::string> documents, unsigned int n, char delim){
 	std::vector<std::string> tokens;
 	std::vector<std::string> ngrams;
+	std::vector<std::map<std::string, int> > ngramFrequency(documents.size()); 
 	std::string str;
 
-	copy(std::istream_iterator<std::string>(iss),
-		std::istream_iterator<std::string>(),
-		back_inserter(tokens));
-
-	for (unsigned int i = n; i <= tokens.size(); ++i){
-		str = tokens[i - n];
-		for (unsigned int j = i - n + 1; j < i; j++){ str = str + ' ' + tokens[j]; }
-		ngrams.push_back(str);
-	}
-	
-	std::sort(ngrams.begin(), ngrams.end());
-
-	return(ngrams);
-}
-
-
-// [[Rcpp::export(name=".tdm_creator")]]
-std::map<std::string, int> tdm_creator(std::vector<std::string> ngrams){
-  std::map<std::string, int> tdm;
-	std::vector<int> matched;
-
-	int count = 1;
-
-	for (unsigned int i = 0; i < ngrams.size() - 1; i++){
+	for(int k = 0; k < documents.size(); k++){
+	  std::transform(documents[k].begin(), documents[k].end(), documents[k].begin(), ::tolower);
 	  
-	  if(ngrams[i] == ngrams[i+1]){
-	    count++;
-	    continue;
-	  }else{
-	    tdm.insert(std::pair<std::string, int>(ngrams[i], count));
-	    count = 1;
+	  std::stringstream ss(documents[k]);
+	  std::string item;
+	  while (std::getline(ss, item, delim)) {
+	    if (!item.empty())
+	      tokens.push_back(item);
 	  }
 	  
-	  
-	  /*
-	  if(matched.size() == ngrams.size() - i){  // if all the remaining words have already been matched, break
-	    break;
+	  for (unsigned int i = n; i <= tokens.size(); i++){
+	    str = tokens[i - n];
+	    for (unsigned int j = i - n + 1; j < i; j++){ str = str + ' ' + tokens[j]; }
+	    ngrams.push_back(str);
 	  }
 	  
-		if (std::find(matched.begin(), matched.end(), i) != matched.end()){  // check element hasn't already been found
-			continue;
-		}
-
-		count = 1;  // initialise count
-		
-		for (unsigned int j = i+1; j < ngrams.size(); j++){
-			if (ngrams[i] == ngrams[j]){
-				count++;	// increment counter variable if you get a match
-				matched.push_back(j);	// save vector of matches to prevent re-reading the same variables
-			}
-		}
-
-		tdm.insert(std::pair<std::string, int>(ngrams[i], count));
-	   */
-	}
-	
-	return(tdm);
-}
-
-
-// [[Rcpp::export(name="tdm_generator")]]
-Rcpp::List tdm_generator(std::vector<std::string> documents, int n){
-  std::vector<std::map<std::string, int> > matches;
-  std::vector<std::string> ngrams;
-	std::vector<std::string> terms;
-
-	for (unsigned int i = 0; i < documents.size(); i++){
-	  ngrams = ngram_generator(documents[i], n);
-	  matches.push_back(tdm_creator(ngrams));
+	  tokens.clear();
 	  
-	  //for (auto elem : matches[i]){
-	  for(std::map<std::string, int>::iterator it = matches[i].begin(); it != matches[i].end(); it++) {
-	    terms.push_back(it->first);  // create vector of all terms
-	    //terms.push_back(elem.first);
-	  }
-	}
-	
-	std::vector<std::string> terms_unique = terms;
-	
-	// find all unique terms
-	std::sort(terms_unique.begin(), terms_unique.end());
-	terms_unique.erase(std::unique(terms_unique.begin(), terms_unique.end()), terms_unique.end());
-
-	const int t = terms_unique.size();
-	const int d = documents.size();
-
-	arma::sp_mat dtm(d,t);
-	int position;
-	
-	for (int i = 0; i < d; i++) {
+	  std::sort(ngrams.begin(), ngrams.end());
+	 
+	  std::vector<int> matched;
 	  
-	  /*
-	  
-	  std::vector<std::string>::iterator beginner = terms_unique.begin();
-	  
-	  //for(auto elem : matches[i]){
-	  for(std::map<std::string, int>::iterator it = matches[i].begin(); it != matches[i].end(); it++){
-	    position = std::find(beginner, terms_unique.end(), it->first) - terms_unique.begin();
-	    //dtm(i, position) = elem.second;
-	    dtm(i,position) = it->second;
-	    beginner++;
-	  }
-	  */
-	  
-	  std::map<std::string, int>::iterator it = matches[i].begin();
-	  for (int j = 0; j < t; j++){
-	    if (it->first == terms_unique[j]){
-	      dtm(i, j) = it->second;
-	      ++it;
-	      if (it == matches[i].end()){
-	        break;
-	      }
+	  int count = 1;
+	  for (unsigned int i = 0; i <= ngrams.size() - 1; i++){
+	    if (i == ngrams.size() - 1){
+	      ngramFrequency[k].insert(std::pair<std::string, int>(ngrams[i], count));
+	      break;
+	    }
+	    if(ngrams[i] == ngrams[i+1]){
+	      count++;
+	      continue;
+	    }else{
+	      ngramFrequency[k].insert(std::pair<std::string, int>(ngrams[i], count));
+	      count = 1;
 	    }
 	  }
+	  
+	  ngrams.clear();
 	}
-  
-	return Rcpp::List::create(Rcpp::Named("terms") = terms_unique, Rcpp::Named("dtm") = dtm);
+	
+	return(ngramFrequency);
 }
-
-// std::vector<std::string>::iterator start = terms.begin();
-// std::vector<std::string>::iterator end = terms.begin() + terms.size() / 2;
-
-int RecursiveTreeSearch(std::vector<std::string> terms, std::string search, std::vector<std::string>::iterator start, std::vector<std::string>::iterator end){
-  int range = end - start;
-  
-  if(range > 0){
-    if(std::binary_search(start,end,search)){
-      
-      TreeSearch()
-      
-    }
-  }else{
-    return
-  }
-}
-
 
